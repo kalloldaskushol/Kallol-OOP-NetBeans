f30e9471b098570f5c347e8b34a6808b2c088bbb
Kallol Das Kushol <kallold70@gmail.com>
2025-12-31 00:27:55 +0600

    normal

---
+++ src/main/java/Generics/GenericModerate.java
@@ -0,0 +1,75 @@
+
+package Generics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author KALLOL DAS KUSHOL
+ */
+public class GenericModerate {
+    
+    <T> void printAtMethodLevel(T val) { // we can use Generic property at method level.
+        System.out.println(val);
+    }
+    
+//    Bounded type:
+    <T extends Number> void Bounded(T val) { // Now we can't use String for this method.
+        System.out.println(val);
+    }
+    
+//    WildCard: -> works for cointainer: Array, List etc..
+    void wildCard(List<?> list){ // ? means anything is acceptable. //Access for everyone.
+        
+//        for(Integer i:list){
+//            System.out.println(i);
+//        } // -> will not work for String
+        
+        for(Object i:list){
+            System.out.println(i);
+        }
+//        list.add("Hello"); // Not Possible bcz WILDCARD is read-only. To ensure type-safety        
+    }
+    
+//    Lower Bound
+    void lowerBound(List<? extends Number> list){ // now we can't use String
+        
+        for(Number i:list){ // Object will also work as it is the superclass
+            System.out.println(i);
+        }
+        
+//        list.add(7); -> read-only
+    }
+    
+//    Upper Bound
+    void upperBound(List<? super Integer> list){ // now we can use only those who are equavalent to Integer or its parent
+        
+        list.add(7);// -> write-able
+        
+        for(Object i:list){ // Object will also work bcz its the most superclass
+            System.out.println(i);
+        }
+        
+    }
+    
+    
+    public static void main(String[] args) {
+        GenericModerate obj1 = new GenericModerate();
+        
+        // obj1.printAtMethodLevel("Hello");
+//        obj1.Bounded("Hello"); // Not Possible
+        // obj1.Bounded(7);
+        
+        // Type of a ArrayList must be class (premitive data type will not be accepted)
+        ArrayList<Integer> al = new ArrayList<>();
+        al.add(7);
+        al.add(10);
+        
+//        ArrayList al2 = new ArrayList();
+//        al2.add(7);
+//        al2.add("ABCD"); // Will also work but it not ensures type safety
+
+        obj1.upperBound(al);
+    }
+}
---
+++ src/main/java/Miscellaneous/GarbageCollector.java
@@ -0,0 +1,20 @@
+
+package Miscellaneous;
+
+/**
+ *
+ * @author KALLOL DAS KUSHOL
+ */
+
+/*
+-What is Garbage Collection?
+    ~ Garbage Collection (GC) is the process of automatic memory management in Java.
+
+- Objects created using new are stored in the Heap memory.
+    new GarbageCollector();
+    new GarbageCollector();
+*/
+
+public class GarbageCollector {
+    
+}
